35de0643cc2e9a92c5c6c89570384075d8c269a1
cryptobfund <cryptobfund@gmail.com>
2020-09-09 21:20:36 +0300

    css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,23 +1,53 @@
 <!DOCTYPE html>
+
 <html>
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <title>@yield('title')</title>
-    <meta charset="UTF-8">
+
+    <!-- Scripts -->
+    <!--<script src="https://php-l3-page-analyzer.herokuapp.com/js/app.js" defer></script> -->
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="./css/app.css" rel="stylesheet">
 </head>
-<body>
-    {{--@include('navbar')--}}
-    <div class="container-fluid">
-        <a href="{{route('welcome')}}">Home</a>
-        <a href="{{route('domains.index')}}">Domains</a>
-        <div class="jumbotron">
-            @yield('content')
+<body class="d-flex flex-column">
+<header>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <a class="navbar-brand" href="{{route('welcome')}}">Analyzer</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" href="{{route('welcome')}}">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="{{route('domains.index')}}">Domains</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
+
+<main class="flex-grow-1">
+    @yield('content')
+</main>
+
+<footer class="border-top py-3 mt-5">
+    <div class="container-lg">
+        <div class="text-center">
+            created by
+            <a target="_blank" href="https://github.com/cryptobfund">Johnik</a>
         </div>
     </div>
-</body>
-<footer class="fixed-bottom">
-    <hr/>
-    <div class="container text-center mt-5 pb-3">
-        created by <a target="_blank" href="https://github.com/cryptobfund">Johnik</a>
-    </div>
 </footer>
+</body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,22 @@
 @section('content')
 
 
-    {{Form::open(['url' => route('domains.store'), 'method' => 'POST'])}}
-    {{Form::url('name')}}
-    {{Form::submit('Check')}}
-    {{Form::close()}}
+    <div class="jumbotron jumbotron-fluid bg-dark">
+        <div class="container-lg">
+            <div class="row">
+                <div class="col-12 col-md-10 col-lg-8 mx-auto text-white">
+                    <h1 class="display-3">Page Analyzer</h1>
+                    <p class="lead">Check web pages for free</p>
+                 <!--   <form method="POST" action="{ route("domains.store") }}"  class="d-flex justify-content-center">
+                        <input type="text" name="name" value="" class="form-control form-control-lg" placeholder="https://www.example.com">
+                        <button type="submit" class="btn btn-lg btn-primary ml-3 px-5 text-uppercase">Check</button>
+                    </form> -->
+                    {{Form::open(['url' => route('domains.store'), 'method' => 'POST', 'class' => "d-flex justify-content-center"])}}
+                    {{Form::url('name')}}
+                    {{Form::submit('Check')}}
+                    {{Form::close()}}
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
